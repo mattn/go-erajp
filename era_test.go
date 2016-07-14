e31351d74405854829f76829837c4e0650c9c4d7
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-07-14 20:17:34 +0900

    first import

---
+++ era_test.go
@@ -0,0 +1,30 @@
+package japanese_era
+
+import (
+	"testing"
+)
+
+var testcases = []struct {
+	year int
+	era  string
+}{
+	{1900, `明治`},
+	{1911, `明治`},
+	{1912, `大正`},
+	{1925, `大正`},
+	{1926, `昭和`},
+	{1988, `昭和`},
+	{1989, `平成`},
+	{2016, `平成`},
+}
+
+func TestSimple(t *testing.T) {
+	for _, testcase := range testcases {
+		year := testcase.year
+		got := ToEra(year)
+		expected := testcase.era
+		if got != expected {
+			t.Fatalf("Expected %v for year %v, but %v:", expected, year, got)
+		}
+	}
+}

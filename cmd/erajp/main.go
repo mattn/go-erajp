6d6d9810ce59f08930f7ad41b817a49c16ed38ba
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-12-28 19:43:49 +0900

    truncate

--- cmd/erajp/main.go
+++ cmd/erajp/main.go
@@ -8,27 +8,46 @@ import (
 	"github.com/mattn/go-erajp"
 )
 
-var formats = []string{
-	"2006",
-	"2006/1",
-	"2006/01",
-	"2006/1/2",
-	"2006/01/02",
+var formats = []struct {
+	f string
+	t int
+}{
+	{"2006", 0},
+	{"2006/1", 1},
+	{"2006/01", 1},
+	{"2006/1/2", 2},
+	{"2006/01/02", 2},
 }
 
 func main() {
 	if len(os.Args) == 1 {
 		ymd := time.Now()
 		if item := erajp.Find(ymd); item != nil {
-			fmt.Printf("%s%d年\n", item.Name, ymd.Year()-item.Year+1)
+			y := fmt.Sprint(ymd.Year() - item.Year + 1)
+			if y == "1" {
+				y = "元"
+			}
+			fmt.Printf("%s%s年\n", item.Name, y)
 			return
 		}
 	} else if len(os.Args) == 2 {
 		for _, format := range formats {
-			ymd, err := time.Parse(format, os.Args[1])
+			ymd, err := time.ParseInLocation(format.f, os.Args[1], time.Local)
 			if err == nil {
+				switch format.t {
+				case 0:
+					ymd = time.Date(ymd.Year()+1, 1, 1, 0, 0, 0, 0, time.Local).Add(-1)
+				case 1:
+					ymd = time.Date(ymd.Year(), ymd.Month()+1, 1, 0, 0, 0, 0, time.Local).Add(-1)
+				case 2:
+					ymd = time.Date(ymd.Year(), ymd.Month(), ymd.Day()+1, 0, 0, 0, 0, time.Local).Add(-1)
+				}
 				if item := erajp.Find(ymd); item != nil {
-					fmt.Printf("%s%d年\n", item.Name, ymd.Year()-item.Year+1)
+					y := fmt.Sprint(ymd.Year() - item.Year + 1)
+					if y == "1" {
+						y = "元"
+					}
+					fmt.Printf("%s%s年\n", item.Name, y)
 					return
 				}
 			}
